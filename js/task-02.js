5b7db5ac0d701a586ab329235fc4f2a861123832
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-05-10 13:49:19 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+	const list = document.createElement("li");
+	list.classList.add("item");
+	list.textContent = ingredient;
+	ul.append(list);
+});
