bba9bd85aa5e88f287e290ae8542f67698ff6c4e
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-05-10 13:14:39 +0200

    first task

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const numOfCat = document.querySelectorAll(".item");
+console.log(`Number of categories: ${numOfCat.length}`);
+
+const firstCat = document.getElementsByTagName("h2")[0];
+console.log(`Category: ${firstCat.innerText}`);
+const firstList = firstCat.nextElementSibling.children;
+console.log(`Elements: ${firstList.length}`);
+
+const SecondCat = document.getElementsByTagName("h2")[1];
+console.log(`Category: ${SecondCat.innerText}`);
+const secondList = SecondCat.nextElementSibling.children;
+console.log(`Elements: ${secondList.length}`);
+
+const thirdCat = document.getElementsByTagName("h2")[2];
+console.log(`Category: ${thirdCat.innerText}`);
+const thirdList = thirdCat.nextElementSibling.children;
+console.log(`Elements: ${thirdList.length}`);
