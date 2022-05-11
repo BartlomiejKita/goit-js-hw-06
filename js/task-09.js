3d9a0a228276f657e414d38b5c8bd20c092e7388
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-05-11 16:12:07 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const color = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", () => {
+	bodyColor.style.backgroundColor = getRandomHexColor();
+	color.textContent = getRandomHexColor();
+});
