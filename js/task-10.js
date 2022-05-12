e41c57dc5b5d6112522100b8173b2c1a39043d73
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-05-12 10:42:26 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const number = document.querySelector("input");
+const btnCreate = document.querySelector("data-create");
+const btnDestroy = document.querySelector("data-destroy");
+const createBox = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+
+function createBoxes() {
+	for (let i = 0; i <= number.value; i++) {
+		let div = document.createElement("div");
+		createBox.append("div");
+	}
 }
